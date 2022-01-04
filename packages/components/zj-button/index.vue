8964ce7b52fd5750a1c6b0ccb943788be1c55b0a
zhoujian <zhoujj-f@glodon.com>
2022-01-04 13:48:44 +0800

    zj-vue-ui

---
+++ packages/components/zj-button/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="zj-button">我是一个按钮组件</button>
+</template>
+
+<script>
+export default {
+  name: "zj-button",
+  data() {
+    return {};
+  },
+  methods: {},
+  filters: {},
+  created() {},
+};
+</script>
+
+<style>
+.zj-button {
+  color: red;
+}
+</style>
